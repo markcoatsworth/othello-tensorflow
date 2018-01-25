65e667b24dfbd75efb0bc62deb56c7a5cc2446e2
Mark Coatsworth <coatsworth@cs.wisc.edu>
2018-01-25 11:28:48 -0600

    Added robot battles to test various strategies

--- game.py
+++ game.py
@@ -98,11 +98,11 @@ class Game(object):
         return move_pos
 
 
-    # [Game.play]
-    # @description: Main game loop. 
+    # [Game.play_human]
+    # @description: Human vs computer main game loop
     # @param1: Self
     # @param2: Which player is human (BLACK or WHITE)
-    def play(self, human_player):
+    def play_human(self, human_player):
 
         self._player_pieces[BLACK] = [28, 35]
         self._player_pieces[WHITE] = [27, 36]
@@ -180,6 +180,67 @@ class Game(object):
         # At this point we're out of the main loop, game is over! First
         print("\n\nGame over!\n")
         self._board.show(self._available_moves[human_player])
+        self.set_player_pieces(BLACK)
+        self.set_player_pieces(WHITE)
+        print("Black has " + str(len(self._player_pieces[BLACK])) + " pieces")
+        print("White has " + str(len(self._player_pieces[WHITE])) + " pieces\n")
+        if len(self._player_pieces[BLACK]) > len(self._player_pieces[WHITE]):
+            print("Black wins!\n\n")
+        elif len(self._player_pieces[WHITE]) > len(self._player_pieces[BLACK]):
+            print("White wins!\n\n")
+        else:
+            print("Game is a draw")
+
+    # [Game.robot_battle]
+    # @description: Computer vs computer main game loop. Use this to test
+    #   different strategies and board weightings.
+    # @param1: Self
+    def robot_battle(self, verbose=True):
+
+        self._player_pieces[BLACK] = [28, 35]
+        self._player_pieces[WHITE] = [27, 36]
+        for p in self._player_pieces[BLACK]: self._board._positions[p] = BLACK
+        for p in self._player_pieces[WHITE]: self._board._positions[p] = WHITE
+        self.set_available_moves(BLACK)
+        self.set_available_moves(WHITE)
+
+        self._player_names[BLACK] = "Black"
+        self._player_names[WHITE] = "White"
+        self._game_turn = BLACK
+
+        # Main game loop
+        while self._game_turn != GAME_OVER:
+
+            current_player = self._game_turn
+            opponent = self._game_turn^3
+            
+            self.set_player_pieces(BLACK)
+            self.set_player_pieces(WHITE)
+
+            if verbose:
+                self._board.show(self._available_moves[current_player])
+                print("\nBlack pieces: " + str(self._player_pieces[BLACK]))
+                print("White pieces: " + str(self._player_pieces[WHITE]))
+                print("Black available moves: " + str(self._available_moves[BLACK]))
+                print("White available moves: " + str(self._available_moves[WHITE]) + "\n")
+
+            # Current player plays a random move
+            move_pos = self.generate_move(current_player)
+            self._board.play_move(current_player, move_pos)
+            if verbose:
+                print(str(self._player_names[current_player]) + " played " + str(chr((move_pos%8)+97)) + str((move_pos//8)+1) + " (position " + str(move_pos) + ")\n")
+            self.set_available_moves(opponent)
+            # Check if other player passes, or if game is over
+            if len(self._available_moves[opponent]) == 0:
+                self.set_available_moves(current_player)
+                if len(self._available_moves[current_player]) == 0:
+                    self._game_turn = GAME_OVER
+            else:
+                self._game_turn = opponent
+
+        # At this point we're out of the main loop, game is over! First
+        print("\n\nGame over!\n")
+        self._board.show(self._available_moves[current_player])
         self.set_player_pieces(BLACK)
         self.set_player_pieces(WHITE)
         print("Black has " + str(len(self._player_pieces[BLACK])) + " pieces")
--- othello.py
+++ othello.py
@@ -2,16 +2,41 @@
 
 from game import Game
 
+import sys
+
 BLACK = 1
 WHITE = 2
 
 
 def main():
 
-    # Human vs computer
+    human_player = BLACK
+    robot_battle = False
+
+    # Read command line args. Assume this is a human vs. computer game, with
+    # human playing first, unless told otherwise.
+    for arg in sys.argv:
+        print("[main] arg=" + str(arg))
+        if(arg == "--help"):
+            print("Options:")
+            print("--white: Human plays white, computer goes first")
+            print("--robot-battle: Computer plays against itself")
+        if(arg == "--white"):
+            human_player = WHITE
+        if(arg == "--robot-battle"):
+            robot_battle = True
+        
+
     game = Game()
-    game.play(BLACK) # Human goes first
-    #game.play(WHITE) # Computer goes first
+
+    # Human vs computer
+    if not robot_battle:
+        game.play_human(human_player)
+
+    # Computer vs computer
+    else:
+        game.robot_battle()
+
 
 if __name__ == "__main__":
     main()
